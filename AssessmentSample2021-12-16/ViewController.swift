b7b065ef1274e77626c2dc5f96d578eeab3a0f00
ryoryoryoooo <P1211065@gmail.com>
2021-12-16 21:58:46 +0900

    first commit

--- AssessmentSample2021-12-16/ViewController.swift
+++ AssessmentSample2021-12-16/ViewController.swift
@@ -9,9 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+    @IBAction func button1(_ sender: Any) {
+
+    }
+
+    @IBAction func button2(_ sender: Any) {
+    }
+
+    @IBAction func button3(_ sender: Any) {
+    }
+
+    @IBAction func button4(_ sender: Any) {
+    }
+
+    @IBAction func button5(_ sender: Any) {
+    }
+
+    @IBAction func button6(_ sender: Any) {
+    }
+
+    @IBAction func button7(_ sender: Any) {
     }
 
 
