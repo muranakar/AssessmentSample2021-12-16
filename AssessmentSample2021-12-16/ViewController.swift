25c2285792602622ffa24960978fc0c6fe61a3c5
muranakar <P1211065@gmail.com>
2021-12-18 00:33:58 +0900

    選択した項目の点数を出力

--- AssessmentSample2021-12-16/ViewController.swift
+++ AssessmentSample2021-12-16/ViewController.swift
@@ -24,7 +24,8 @@ class ViewController: UIViewController {
     var textsSample: [String] = []
     var buttons: [UIButton] = []
     var dictionary: [UIButton: String] = [:]
-
+    var buttonsNum: [UIButton: Int] = [:]
+    var fimNum = [1,2,3,4,5,6,7]
     override func viewDidLoad() {
         super.viewDidLoad()
         decoderFimJsonFile()
@@ -50,6 +51,7 @@ class ViewController: UIViewController {
             button1,button2,button3,button4,button5,button6,button7
         ]
         dictionary = {[UIButton: String](uniqueKeysWithValues: zip(buttons, textsSample)) }()
+        buttonsNum = {[UIButton: Int](uniqueKeysWithValues: zip(buttons,fimNum))}()
     }
 
     //　ボタンと文字列を関連付けさせた配列を、ボタンが選択された際に、そのボタンと関連づけられた文字列を、テキストビューに反映させる。
@@ -65,6 +67,11 @@ class ViewController: UIViewController {
     //　決定ボタンのアクション
     @IBAction func decide(_ sender: Any) {
         fimItemCount += 1
+        let button = buttons.filter{$0.isSelected == true}.first
+        //配列から、ボタンを取り出して、単体にしてから、↓に代入する。
+        guard let num = buttonsNum[button!] else { return }
+        print(num)
+
         viewWillAppear(true)
         buttons.forEach{ (button: UIButton) in
             button.isSelected = false
@@ -147,7 +154,29 @@ class ViewController: UIViewController {
         } catch {
             fatalError("パース不可")
         }
-
     }
-}
 
+    struct FIM {
+        var uuidString = UUID().uuidString
+        var eating = 0
+        var grooming = 0
+        var bathing = 0
+        var dressingUpperBody = 0
+        var dressingLowerBody  = 0
+        var toileting = 0
+        var bladderManagement = 0
+        var bowelManagement = 0
+        var transfersBedChairWheelchair = 0
+        var transfersToilet = 0
+        var transfersBathShower = 0
+        var walkWheelchair = 0
+        var stairs = 0
+        var comprehension = 0
+        var expression = 0
+        var socialInteraction = 0
+        var problemSolving = 0
+        var memory = 0
+        var createdAt: Date? = nil
+        var updatedAt: Date? = nil
+ }
+}
